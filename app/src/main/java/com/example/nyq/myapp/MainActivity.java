f0e6db9ce6d5b0f396939e4df2813d69ec3f91dd
DESKTOP-153ACLA\nyq <940725773@qq.com>
2018-05-02 19:42:21 +0800

    添加依赖

--- app/src/main/java/com/example/nyq/myapp/MainActivity.java
+++ app/src/main/java/com/example/nyq/myapp/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.nyq.myapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.special.ResideMenu.ResideMenu;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ResideMenu resideMenu = new ResideMenu(this);
     }
 }
