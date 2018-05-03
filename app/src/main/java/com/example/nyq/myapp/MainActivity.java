db7c339aff95f40418696cd018e27c5356ddb792
DESKTOP-153ACLA\nyq <940725773@qq.com>
2018-05-03 10:01:51 +0800

    侧滑界面。

--- app/src/main/java/com/example/nyq/myapp/MainActivity.java
+++ app/src/main/java/com/example/nyq/myapp/MainActivity.java
@@ -1,17 +1,72 @@
 package com.example.nyq.myapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.special.ResideMenu.ResideMenu;
+import com.special.ResideMenu.ResideMenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ResideMenu resideMenu;
+    private ResideMenuItem addItem;
+    private ResideMenuItem selectItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        ResideMenu resideMenu = new ResideMenu(this);
+        setUpMenu();
+    }
+
+    private ResideMenu.OnMenuListener menuListener = new ResideMenu.OnMenuListener() {
+        @Override
+        public void openMenu() {
+            Toast.makeText(MainActivity.this, "is open", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void closeMenu() {
+            Toast.makeText(MainActivity.this, "is close", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+
+    private void setUpMenu() {
+        resideMenu = new ResideMenu(getApplicationContext());
+        resideMenu.attachToActivity(this);
+        resideMenu.setBackground(R.mipmap.menu_background);
+        resideMenu.setScaleValue(0.6f);
+        resideMenu.setUse3D(true);
+        resideMenu.setMenuListener(menuListener);
+
+        addItem = new ResideMenuItem(getApplicationContext(), R.mipmap.icon_home, "新增");
+        selectItem = new ResideMenuItem(getApplicationContext(), R.mipmap.icon_profile, "查找");
+
+        resideMenu.addMenuItem(addItem, ResideMenu.DIRECTION_LEFT);
+        resideMenu.addMenuItem(selectItem, ResideMenu.DIRECTION_LEFT);
+
+        addItem.setOnClickListener(this);
+
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        return resideMenu.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (addItem == view) {
+
+        }
+
     }
 }
