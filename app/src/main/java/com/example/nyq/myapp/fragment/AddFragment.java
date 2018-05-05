c1bb74f58257243940a017c78a72d441b26e2a90
DESKTOP-153ACLA\nyq <940725773@qq.com>
2018-05-05 16:54:42 +0800

    扫码生成二维码集成

--- app/src/main/java/com/example/nyq/myapp/fragment/AddFragment.java
+++ app/src/main/java/com/example/nyq/myapp/fragment/AddFragment.java
@@ -1,22 +1,71 @@
 package com.example.nyq.myapp.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.dommy.qrcode.ZingMainActivity;
+import com.dommy.qrcode.util.Constant;
 import com.example.nyq.myapp.R;
+import com.google.zxing.activity.CaptureActivity;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Created by nyq on 2018/5/3.
  */
 
-public class AddFragment extends Fragment {
+public class AddFragment extends Fragment implements View.OnClickListener {
+
+    Button btnQrCode; // 扫码
+    TextView tvResult; // 结果
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-      return  inflater.inflate(R.layout.add_fragment, container, false);
+        View view = inflater.inflate(R.layout.add_fragment2, container, false);
+
+        initView(view);
+        return view;
+    }
+
+    private void initView(View view) {
+        btnQrCode = (Button) view.findViewById(com.dommy.qrcode.R.id.btn_qrcode);
+        btnQrCode.setOnClickListener(this);
+
+        tvResult = (TextView) view.findViewById(com.dommy.qrcode.R.id.txt_result);
+    }
+    // 开始扫码
+    private void startQrCode() {
+
+        // 二维码扫码
+        Intent intent = new Intent(getContext(), CaptureActivity.class);
+        startActivityForResult(intent, Constant.REQ_QR_CODE);
+    }
+    @Override
+    public void onClick(View view) {
+        int i = view.getId();
+        if (i == com.dommy.qrcode.R.id.btn_qrcode) {
+            startQrCode();
+
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //扫描结果回调
+        if (requestCode == Constant.REQ_QR_CODE && resultCode == RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            String scanResult = bundle.getString(Constant.INTENT_EXTRA_KEY_QR_SCAN);
+            //将扫描出的信息显示出来
+            tvResult.setText(scanResult);
+        }
     }
 }
---
+++ zinglibrary/src/main/java/com/dommy/qrcode/ZingMainActivity.java
@@ -0,0 +1,93 @@
+package com.dommy.qrcode;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.dommy.qrcode.util.Constant;
+import com.google.zxing.activity.CaptureActivity;
+
+public class ZingMainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnQrCode; // 扫码
+    TextView tvResult; // 结果
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.zing_activity_main);
+        ActivityCompat.requestPermissions(ZingMainActivity.this, new String[]{Manifest.permission.CAMERA,Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE}, Constant.REQ_PERM_CAMERA);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // 申请权限
+            ActivityCompat.requestPermissions(ZingMainActivity.this, new String[]{Manifest.permission.CAMERA,Manifest.permission.READ_EXTERNAL_STORAGE}, Constant.REQ_PERM_CAMERA);
+            return;
+        }
+        initView();
+    }
+
+    private void initView() {
+        btnQrCode = (Button) findViewById(R.id.btn_qrcode);
+        btnQrCode.setOnClickListener(this);
+
+        tvResult = (TextView) findViewById(R.id.txt_result);
+    }
+
+    // 开始扫码
+    private void startQrCode() {
+
+        // 二维码扫码
+        Intent intent = new Intent(ZingMainActivity.this, CaptureActivity.class);
+        startActivityForResult(intent, Constant.REQ_QR_CODE);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int i = view.getId();
+        if (i == R.id.btn_qrcode) {
+            startQrCode();
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //扫描结果回调
+        if (requestCode == Constant.REQ_QR_CODE && resultCode == RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            String scanResult = bundle.getString(Constant.INTENT_EXTRA_KEY_QR_SCAN);
+            //将扫描出的信息显示出来
+            tvResult.setText(scanResult);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+
+            case Constant.REQ_PERM_CAMERA:
+
+                // 摄像头权限申请
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 获得授权
+                    startQrCode();
+                } else {
+                    // 被禁止授权
+                    Toast.makeText(ZingMainActivity.this, "请至权限中心打开本应用的相机访问权限", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
+
+
+}
